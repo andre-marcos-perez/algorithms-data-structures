1d2ad916e208b73923ca624b352c6eba5c82cef4
Andre Perez <andre.perez@sumup.com>
2024-11-07 23:24:35 +0100

    create linked list tests

--- Makefile
+++ Makefile
@@ -3,10 +3,14 @@ help: ## Show this help message
 	@grep -h -E '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 .PHONY: format
-format: ## Format code using isort and black
+format: ## Format code
 	@isort structures --profile black
 	@black structures
 
 .PHONY: lint
-lint: ## Lint code using flake8
+lint: ## Lint code
 	@flake8 structures --count --show-source --statistics --max-line-length 88
+
+.PHONY: test
+test: ## Run unit tests
+	@python3 -m unittest discover -v -s structures/sequences -p "*_test.py"
---
+++ structures/sequences/linked_list_test.py
@@ -0,0 +1,79 @@
+import unittest
+
+from structures.sequences.linked_list import LinkedList
+
+
+class TestContainer(unittest.TestCase):
+
+    def setUp(self):
+        self.collection = (1, 2, 3)
+        self.struct = LinkedList()
+        self.struct.build(self.collection)
+
+    def test_len(self):
+        self.assertEqual(len(self.struct), len(self.collection))
+
+    def test_build(self):
+        for i, item in enumerate(self.struct):
+            self.assertEqual(item, self.collection[i])
+
+
+class TestStaticOperations(unittest.TestCase):
+
+    def setUp(self):
+        self.collection = (1, 2, 3)
+        self.struct = LinkedList()
+        self.struct.build(self.collection)
+
+    def test_get(self):
+        item = self.struct.get(2)
+        self.assertEqual(item, 3)
+
+    def test_set(self):
+        self.struct.set(2, 9)
+        item = self.struct.get(2)
+        self.assertEqual(item, 9)
+
+
+class TestDynamicOperations(unittest.TestCase):
+
+    def setUp(self):
+        self.collection = (1, 2, 3)
+        self.struct = LinkedList()
+        self.struct.build(self.collection)
+
+    def test_insert_first(self):
+        self.struct.insert_first(9)
+        item = self.struct.get(0)
+        self.assertEqual(item, 9)
+
+    def test_delete_first(self):
+        item = self.struct.get(0)
+        deleted = self.struct.delete_first()
+        self.assertEqual(deleted, item)
+
+    def test_insert_last(self):
+        self.struct.insert_last(7)
+        item = self.struct.get(len(self.struct) - 1)
+        self.assertEqual(item, 7)
+
+    def test_delete_last(self):
+        item = self.struct.get(len(self.struct) - 1)
+        deleted = self.struct.delete_last()
+        self.assertEqual(deleted, item)
+
+    def test_insert(self):
+        self.struct.insert(1, 9)
+        # prev
+        item = self.struct.get(0)
+        self.assertEqual(item, 1)
+        # current
+        item = self.struct.get(1)
+        self.assertEqual(item, 9)
+        # next
+        item = self.struct.get(2)
+        self.assertEqual(item, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
